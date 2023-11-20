a92eba774c8df778b47c73f96e1f92c7a1158603
Kevin <kjrodgers26@gmail.com>
2023-11-19 23:36:23 -0500

    added assesr and styles to landing page

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -3,24 +3,41 @@ import headerImg from "../../assets/best-female-friends-spending-time-together-r
 import LandingNav from "./LandingNav"
 import ReactTyped from "react-typed"
 import { names } from "../../constants/names"
+import { motion } from "framer-motion"
 
 const LandingPage = () => {
   return (
     <div className="h-screen">
       <LandingNav />
-      <h1 className="text-center text-[10rem]">
-        Do{" "}
-        <ReactTyped
-          strings={names}
-          typeSpeed={90}
-          backSpeed={40}
-          loop
-          className="text-[#FF8356]"
-        />
-        <br /> a <span className="text-[#F54D42]">Solid</span>
-      </h1>
-      <div className="absolute bottom-0 max-w-[75rem] left-0 right-0 m-auto">
-        <img src={headerImg} alt="Two friends" className="bg-center" />
+      <div className="flex flex-col items-center text-center justify-center">
+        <div className="text-center rounded-full py-2 px-6 bg-[#FF8356] w-fit justify-center items-center">
+          Help out your pals!
+        </div>
+      </div>
+      <div className="flex justify-between items-center h-screen">
+        <h1 className="text-center text-[5rem] w-1/2">
+          Do{" "}
+          <ReactTyped
+            strings={names}
+            typeSpeed={90}
+            backSpeed={40}
+            loop
+            className="text-[#FF8356]"
+          />
+          <br /> a <span className="text-[#F54D42]">Solid</span>
+        </h1>
+        <motion.div
+          initial={{ scale: 0, y: 200 }}
+          animate={{ y: 0, scale: 1 }}
+          transition={{
+            type: "spring",
+            duration: 2,
+            ease: "linear",
+          }}
+          className="w-[40rem] items-center"
+        >
+          <img src={headerImg} alt="Two friends" className="bg-center" />
+        </motion.div>
       </div>
     </div>
   )
