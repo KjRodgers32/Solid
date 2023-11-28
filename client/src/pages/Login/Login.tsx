c8ce344a3ac6dc9f9f3243b1dd7c7a3452f0e2a7
Kevin <kjrodgers26@gmail.com>
2023-11-27 23:20:40 -0500

    added starter code for server

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,24 +1,35 @@
 import React from "react"
+import logo from "../../assets/Solid (1)-fotor-bg-remover-20231118221321.png"
 
 const Login = () => {
   return (
-    <>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mx-auto">
-        <div className="w-[25rem] h-[25rem] rounded-md shadow-2xl border">
-          <form action="">
-            <div className="flex flex-col">
-              <label>Email</label>
-              <input type="email" />
+    <div>
+      <div className="h-screen w-full">
+        <div className="flex">
+          <div className="w-1/3 h-screen items-center mt-20">
+            <div className="flex flex-col items-center w-full">
+              <div className="w-[10rem]">
+                <img src={logo} alt="logo" />
+              </div>
+              <div className="w-full text-center">
+                <h1 className="text-3xl font-bold">Welcome</h1>
+                <h2 className="font-bold">Do Your Friend A Solid</h2>
+                <h2 className="font-bold mt-16 text-left ml-5">
+                  Sign up or Login
+                </h2>
+              </div>
+              <div className="w-[70%] mt-5">
+                <form className="flex flex-col text-left">
+                  <input type="email" placeholder="Enter Your Email" />
+                  <input type="text" placeholder="Enter Your Password" />
+                </form>
+              </div>
             </div>
-            <div className="flex flex-col">
-              <label>Password</label>
-              <input type="text" />
-            </div>
-            <button>Login</button>
-          </form>
+          </div>
+          <div className="w-2/3 bg-blue-900 h-screen"></div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
---
+++ server/server.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+	res.status(200).json({
+		status: "success",
+		message: "Hello, world",
+	});
+});
+
+app.listen(3000, () => {
+	console.log("listening on port 3000");
+});
